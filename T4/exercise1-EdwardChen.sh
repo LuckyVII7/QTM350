783ac990cdccf87ffbceeb995459332b238cd1da
LuckyVII7 <78717304+LuckyVII7@users.noreply.github.com>
2021-10-04 23:18:01 -0400

    Update exercise1-EdwardChen.sh

--- T4/exercise1-EdwardChen.sh
+++ T4/exercise1-EdwardChen.sh
@@ -2,6 +2,6 @@
 
 clear
 
-PTYPE=$( ls -l "$1" | cut -d '' -f 1 | tail -n +2 | wc -l )
+PTYPE=$( ls -l "$1" | cut -d '' -f 1 | tail -n +2 | sort -n | uniq -c | wc -l )
 
 echo "There are ${PTYPE} number of different permission types in $1 directory."
